8fba4f33e1127b84175bf595c7d1f68a48594c46
jericocarlos <jericonielc@gmail.com>
2026-03-09 08:26:21 +0800

    Update api/attendance/add/route.js.Remove Updating is_enabled to interns and trainees.

--- app/api/attendance/add/route.js
+++ app/api/attendance/add/route.js
@@ -93,26 +93,6 @@ export async function POST(request) {
           [employee.ashima_id]
         );
 
-        // 2️⃣ Update Trainees on HR DB (Server B)
-        // await freemealConn.execute(
-        //   `
-        //   UPDATE trainees
-        //   SET is_enabled = 1
-        //   WHERE ashima_id = ?
-        //   `,
-        //   [employee.ashima_id]
-        // );
-
-        // 2️⃣ Update Interns on HR DB (Server B)
-        // await freemealConn.execute(
-        //   `
-        //   UPDATE interns
-        //   SET is_enabled = 1
-        //   WHERE id_number = ?
-        //   `,
-        //   [employee.ashima_id]
-        // );
-
         await attendanceConn.commit();
         await freemealConn.commit();
 
